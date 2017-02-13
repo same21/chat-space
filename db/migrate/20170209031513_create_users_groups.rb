93fddb9c0fd379a5ead074c0e09227d353bd166b
same21 <SAMEx@sameshimatakahide-no-MacBook-Air.local>
2017-02-13 18:59:21 +0900

    fixed view

--- db/migrate/20170209031513_create_users_groups.rb
+++ db/migrate/20170209031513_create_users_groups.rb
@@ -1,7 +1,7 @@
 class CreateUsersGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :users_groups do |t|
-      t.references :users, index: true, foreign_key: true
+      t.references :users,  index: true, foreign_key: true
       t.references :groups, index: true, foreign_key: true
 
       t.timestamps
