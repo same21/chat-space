28e62624630ee61ec1d82f1800d0303f5f3814fc
same21 <SAMEx@sameshimatakahide-no-MacBook-Air.local>
2017-02-17 20:52:08 +0900

    fixed redirect

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,7 +7,7 @@ end
 def create
   @group = Group.create(group_params)
   if @group.save
-    redirect_to controller: :users, action: :index,notice: "チャットグループが作成されました。"
+    redirect_to root_path,notice: "チャットグループが作成されました。"
   else
     flash[:alert] = "チャットグループの作成に失敗しました。"
     render :new
