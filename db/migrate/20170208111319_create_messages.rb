fb77b9f465ff2b8da78b53e78868efc3e6b35f91
same21 <SAMEx@sameshimatakahide-no-MacBook-Air.local>
2017-02-24 15:37:25 +0900

    fixed migrate

--- db/migrate/20170208111319_create_messages.rb
+++ db/migrate/20170208111319_create_messages.rb
@@ -3,8 +3,8 @@ class CreateMessages < ActiveRecord::Migration[5.0]
      create_table :messages do |t|
       t.text      :body
       t.string    :image
-      t.references :users,  index: true, foreign_key: true
-      t.references :groups, index: true, foreign_key: true
+      t.references :user,  index: true, foreign_key: true
+      t.references :group, index: true, foreign_key: true
       t.timestamps
     end
   end
