121e47a7fdca680af56deb008b9bd09dea78bc97
same21 <SAMEx@sameshimatakahide-no-MacBook-Air.local>
2017-02-25 18:54:53 +0900

    Add  message function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,29 @@
 class MessagesController < ApplicationController
+
+  def set_action
+    @message = Message.new
+    @group = Group.find(params[:group_id])
+  end
+
+  def index
+    set_action
+  end
+
+  def create
+    @message = Message.new(create_params)
+    if @message.save
+      set_action
+      flash.now[:notice] = "メッセージを送信しました。"
+      render :index
+    else
+      set_action
+      flash.now[:alert] = "メッセージ送信に失敗しました。"
+      render :index
+    end
+  end
+
+  private
+    def create_params
+     params.require(:message).permit(:body).merge(group_id: params[:group_id], user_id: current_user.id)
+    end
 end
